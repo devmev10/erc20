654fff1124c17a64497e6d1b6d1f8bc29dbb7c5e
Ravi Tokas <ravitokas90@gmail.com>
2023-08-31 16:39:55 +0530

    update myerc20.sol

--- contracts/myerc20.sol
+++ contracts/myerc20.sol
@@ -1,4 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract myerc20 {}
+contract myerc20 {
+    string name public;
+    string symbol public;
+    uint256 decimals public;
+    uint256 totalSupply public;
+}
