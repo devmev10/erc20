559add0c75205bec1d436d0f4b8153a08890a51c
Ravi Tokas <ravitokas90@gmail.com>
2023-08-31 16:45:09 +0530

    declare total supply and name,symbols

--- contracts/myerc20.sol
+++ contracts/myerc20.sol
@@ -4,6 +4,16 @@ pragma solidity ^0.8.20;
 contract myerc20 {
     string name public;
     string symbol public;
-    uint256 decimals public;
+    uint256 decimals public = 18;
     uint256 totalSupply public;
+
+    mapping (address => uint) balanceOf;
+
+    constructor(){
+        name = "poorcoin";
+        symbol= "poor";
+        totalSupply = 1000000;
+    }
+
+    
 }
